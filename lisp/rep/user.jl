3b1523103c4765bc85031fa79744860f079c875f
John Harper <jsh@unfactored.org>
2012-11-05 18:54:41 -0800

    user module avoids creating top-level bindings when parsing options
    	* rep/user.jl: avoid creating externally-visible bindings

--- lisp/rep/user.jl
+++ lisp/rep/user.jl
@@ -30,39 +30,66 @@
 	   rep.io.processes)
      (set-binds))
 
-  (defun do-load (name)
-    (cond ((file-exists-p name)
-	   (load name nil t t))
-	  ((string-match "\\.jlc?$" name)
-	   (load name))
-	  (t (require (intern name)))))
+  (setq *user-structure* 'user)
 
-  (defun parse-options ()
-    (let (arg)
-      (condition-case error-data
-	  (while (setq arg (car command-line-args))
-	    (setq command-line-args (cdr command-line-args))
-	    (cond
-	     ((member arg '("--call" "-f"))
-	      (setq arg (car command-line-args))
-	      (setq command-line-args (cdr command-line-args))
-	      ((symbol-value (read-from-string arg))))
-	     ((member arg '("--load" "-l"))
-	      (setq arg (car command-line-args))
-	      (setq command-line-args (cdr command-line-args))
-	      (do-load arg))
-	     ((member arg '("-s" "--scheme"))
-	      (setq arg (car command-line-args))
-	      (setq command-line-args (cdr command-line-args))
-	      (setq batch-mode t)
-	      (if (file-exists-p arg)
-		  (structure () (open scheme) (load arg '() 1 1))
-		(structure () (open scheme) (load arg))))
-	     ((string= arg "--check")
-	      (require 'rep.test.framework)
-	      (run-self-tests-and-exit))
-	     ((string= arg "--help")
-	      (format standard-error "\
+  ;; Install all autoload hooks.
+  (load-all "autoload" (lambda (f) (load f nil t)))
+
+  ;; Do operating-system initialisation
+  (load-all (concat "os-" (symbol-name operating-system)) t)
+  
+  ;; Load site specific initialisation. Errors here are trapped since
+  ;; they're probably not going to result in an unusable state
+  (unless (get-command-line-option "--no-rc")
+    (condition-case error-data
+	(progn
+	  ;; First the site-wide stuff
+	  (load-all "site-init")
+	  ;; Now try to interpret the user's startup file, or failing that
+	  ;; the default.jl file providing site-wide user options
+	  (or
+	   (load (concat (user-home-directory) ".reprc") t t t)
+	   (load "rep-default" t)))
+      (error
+       (default-error-handler (car error-data) (cdr error-data)))))
+
+  ;; Avoid creating global definitions in user structure
+
+  (let* ((do-load
+	  (lambda (name)
+	    (cond ((file-exists-p name)
+		   (load name nil t t))
+		  ((string-match "\\.jlc?$" name)
+		   (load name))
+		  (t (require (intern name))))))
+
+	 (parse-options
+	  (lambda ()
+	    (let (arg)
+	      (condition-case error-data
+		  (while (setq arg (car command-line-args))
+		    (setq command-line-args (cdr command-line-args))
+		    (cond
+		     ((member arg '("--call" "-f"))
+		      (setq arg (car command-line-args))
+		      (setq command-line-args (cdr command-line-args))
+		      ((symbol-value (read-from-string arg))))
+		     ((member arg '("--load" "-l"))
+		      (setq arg (car command-line-args))
+		      (setq command-line-args (cdr command-line-args))
+		      (do-load arg))
+		     ((member arg '("-s" "--scheme"))
+		      (setq arg (car command-line-args))
+		      (setq command-line-args (cdr command-line-args))
+		      (setq batch-mode t)
+		      (if (file-exists-p arg)
+			  (structure () (open scheme) (load arg '() 1 1))
+			(structure () (open scheme) (load arg))))
+		     ((string= arg "--check")
+		      (require 'rep.test.framework)
+		      (run-self-tests-and-exit))
+		     ((string= arg "--help")
+		      (format standard-error "\
 usage: %s [OPTIONS...]
 
 where OPTIONS are any of:
@@ -88,59 +115,37 @@ where OPTIONS are any of:
     --version		print version details
     --no-rc		don't load rc or site-init files
     --quit, -q		terminate the interpreter process\n" program-name)
-	      (throw 'quit 0))
-	     ((string= arg "--version")
-	      (format standard-output "rep version %s\n" rep-version)
-	      (throw 'quit 0))
-	     ((member arg '("--quit" "-q"))
-	      (throw 'quit 0))
-	     (t
-	      (setq batch-mode t)
-	      (do-load arg))))
-	(error
-	 (error-handler-function (car error-data) (cdr error-data))
-	 (throw 'quit 1)))))
+		      (throw 'quit 0))
+		     ((string= arg "--version")
+		      (format standard-output "rep version %s\n" rep-version)
+		      (throw 'quit 0))
+		     ((member arg '("--quit" "-q"))
+		      (throw 'quit 0))
+		     (t
+		      (setq batch-mode t)
+		      (do-load arg))))
+		(error
+		 (declare (special error-handler-function))
+		 (error-handler-function (car error-data) (cdr error-data))
+		 (throw 'quit 1)))))))
 
-  (setq *user-structure* 'user)
-
-  ;; Install all autoload hooks.
-  (load-all "autoload" (lambda (f) (load f nil t)))
-
-  ;; Do operating-system initialisation
-  (load-all (concat "os-" (symbol-name operating-system)) t)
-  
-  ;; Load site specific initialisation. Errors here are trapped since
-  ;; they're probably not going to result in an unusable state
-  (unless (get-command-line-option "--no-rc")
-    (condition-case error-data
+    ;; Use all arguments which are left.
+    (if (get-command-line-option "--debug")
 	(progn
-	  ;; First the site-wide stuff
-	  (load-all "site-init")
-	  ;; Now try to interpret the user's startup file, or failing that
-	  ;; the default.jl file providing site-wide user options
-	  (or
-	   (load (concat (user-home-directory) ".reprc") t t t)
-	   (load "rep-default" t)))
-      (error
-       (default-error-handler (car error-data) (cdr error-data)))))
-  
-  ;; Use all arguments which are left.
-  (if (get-command-line-option "--debug")
-      (progn
-	(require 'rep.lang.debugger)
-	(call-with-lexical-origins
-	 (lambda ()
-	   (setq interpreted-mode t)
-	   (setq debug-on-error '(bad-arg missing-arg invalid-function
-				  void-value invalid-read-syntax
-				  premature-end-of-stream invalid-lambda-list
-				  invalid-macro invalid-autoload no-catcher
-				  file-error invalid-stream setting-constant
-				  process-error arith-error
-				  assertion-failed check-failed test-failed))
-	   (break)
-	   (parse-options))))
-    (parse-options))
+	  (require 'rep.lang.debugger)
+	  (call-with-lexical-origins
+	   (lambda ()
+	     (setq interpreted-mode t)
+	     (setq debug-on-error
+		   '(bad-arg missing-arg invalid-function void-value
+		     invalid-read-syntax premature-end-of-stream
+		     invalid-lambda-list invalid-macro invalid-autoload
+		     no-catcher file-error invalid-stream setting-constant
+		     process-error arith-error assertion-failed
+		     check-failed test-failed))
+	     (break)
+	     (parse-options))))
+      (parse-options)))
 
   (unless batch-mode
     (format standard-output "rep %s, Copyright (C) 1999-2000 John Harper
